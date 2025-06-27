1563c5bc9ebdae539dd978d8b7e846bbef239adc
KevnBeauregard <kevin.beauregard.42@hotmail.com>
2025-06-27 04:20:13 -0400

    Delete app directory

--- app/core/config.py
+++
@@ -1,7 +0,0 @@
-WATCHLIST_FILE = "watchlist.json"
-ALERT_LOG_FILE = "alerts.log"
-
-# IFTTT Config
-IFTTT_KEY = "YOUR_IFTTT_KEY"
-IFTTT_EVENT = "breakout_alert"
-IFTTT_URL = f"https://maker.ifttt.com/trigger/{IFTTT_EVENT}/with/key/{IFTTT_KEY}"
